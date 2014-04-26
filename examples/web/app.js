f65e0a0c8af4dd244faebaa19d785d478cdbe859
Joey Guerra <joey@joeyguerra.com>
2014-04-26 09:20:14 -0500

    adding functions for readability.

--- examples/web/app.js
+++ examples/web/app.js
@@ -21,51 +21,59 @@ Web.server = Http.createServer(Web.filter);
 	if(Web[event]) Web.server.on(event, Web[event]);
 });
 
-process.on('uncaughtException', function(err){
-    console.log('uncaughtException: ', err.stack);
-	process.exit(1);
-});
-process.on('exit', function() {
-	Web.server.close(function(){
-		console.log('server is stopping after an exit event ', arguments);
+(function handleProcessExitEvents(){
+	process.on('uncaughtException', function(err){
+	    console.log('uncaughtException: ', err.stack);
+		process.exit(1);
 	});
-	console.log('exited.');
-});
-process.on('SIGTERM', function(){
-	Web.server.close(function(){
-		console.log('server is stopping after an exit event ', arguments);
-	});
-	console.log('SIGTERM.');
-});
-Represent.appPath = appPath;
-Represent.themeRoot = appPath + '/themes/' + process.env.THEME;
-Represent.templatesRoot = Represent.themeRoot + '/templates/';
-Represent.contenTypesFolder = libPath + 'lib/contentTypes';
-Fs.readdirSync(Represent.contenTypesFolder).forEach(function(file) {
-    var contentType = require(Represent.contenTypesFolder + "/" + file);
-    Represent.contentTypes[contentType.key] = contentType;
-});
-
-// Load resources.
-Fs.readdirSync(resourcesFolder).forEach(function(file) {
-	require(resourcesFolder + "/" + file)(Represent.endpoints);
-});
-// Serve up requested files from public.
-Web.hooks.push({
-	handles: function(request){
-		return /\/public\//.test(request.url);
-	}
-	, execute: function staticServer(request, response, next){
-		var path = appPath + '/themes/' + process.env.THEME + request.url.replace('/public', '');
-		Fs.exists(path, function(exists){			
-			if(!exists) return next();
-			var parsed = Url.parse(request.url, true, true);
-			var ext = Path.extname(parsed.pathname);
-			var contentType = Mime.lookup(ext);
-			response.writeHead(200, {'Content-Type':contentType});			
-			Fs.createReadStream(path).pipe(response);
+	process.on('exit', function() {
+		Web.server.close(function(){
+			console.log('server is stopping after an exit event ', arguments);
 		});
-	}
-});
+		console.log('exited.');
+	});
+	process.on('SIGTERM', function(){
+		Web.server.close(function(){
+			console.log('server is stopping after an exit event ', arguments);
+		});
+		console.log('SIGTERM.');
+	});
+})();
+
+(function configureRepresent(){
+	Represent.appPath = appPath;
+	Represent.themeRoot = appPath + '/themes/' + process.env.THEME;
+	Represent.templatesRoot = Represent.themeRoot + '/templates/';
+	Represent.contenTypesFolder = libPath + 'lib/contentTypes';
+	Fs.readdirSync(Represent.contenTypesFolder).forEach(function(file) {
+	    var contentType = require(Represent.contenTypesFolder + "/" + file);
+	    Represent.contentTypes[contentType.key] = contentType;
+	});
+})();
+
+(function loadResourcesFromFolder(){
+	Fs.readdirSync(resourcesFolder).forEach(function(file) {
+		require(resourcesFolder + "/" + file)(Represent.endpoints);
+	});
+})();
+
+(function addStaticServerHook(){
+	Web.hooks.push({
+		handles: function(request){
+			return /\/public\//.test(request.url);
+		}
+		, execute: function staticServer(request, response, next){
+			var path = appPath + '/themes/' + process.env.THEME + request.url.replace('/public', '');
+			Fs.exists(path, function(exists){			
+				if(!exists) return next();
+				var parsed = Url.parse(request.url, true, true);
+				var ext = Path.extname(parsed.pathname);
+				var contentType = Mime.lookup(ext);
+				response.writeHead(200, {'Content-Type':contentType});			
+				Fs.createReadStream(path).pipe(response);
+			});
+		}
+	});
+})();
 Web.hooks.push(require(libPath + 'lib/hook'));
 Web.server.listen(Web.port, Web.listening);
